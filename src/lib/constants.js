b6c13d6ff98df3e0106f4b890bce1a801271fb5f
Hugo Carvalho <hhac@protonmail.com>
2024-05-29 00:03:53 +0100

    Adds a constants class

---
+++ src/lib/constants.js
@@ -0,0 +1,8 @@
+export const DEFAULT_PAGE_NUMBER = 1;
+export const DEFAULT_PAGE_SIZE = 14;
+
+export const BOOK_BASE_URL = 'http://localhost:4000/book/all?pageNumber=1&pageSize=14';
+export const BOOK_QUERY_URL = 'http://localhost:4000/book/search?';
+
+export const AUTHOR_BASE_URL = 'http://localhost:4000/author/all?pageNumber=1&pageSize=14';
+export const AUTHOR_QUERY_URL = 'http://localhost:4000/author/search?';
