b2a0f2b7a56b23fabb0bea216e034382835113fb
Hugo Carvalho <hhac@protonmail.com>
2024-11-10 21:51:34 +0000

    Replaces the use of Axios with the Fetch API

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,22 +1,13 @@
-import axios from 'axios';
-
 const apiUrl = process.env.ENVIRONMENT
 	? import.meta.env.VITE_API_URL_PRODUCTION
 	: import.meta.env.VITE_API_URL_DEVELOPMENT;
 
 export async function get(endpoint) {
 	try {
-		const res = await axios.get(apiUrl + endpoint, {
-			validateStatus: function (status) {
-				return status < 500;
-			},
-		});
+		const request = await fetch(apiUrl + endpoint);
+		const result = await request.json();
 
-		if (res.status !== 200) {
-			return { error: res.statusText + ': ' + res.data.error };
-		}
-
-		return res.data;
+		return result;
 	} catch (error) {
 		return { error: 'Internal Server Error: Network Error' };
 	}
