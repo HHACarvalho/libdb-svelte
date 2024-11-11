6968b51ee21a355075e044d7c31c591cb13ae56a
Hugo Carvalho <hhac@protonmail.com>
2024-11-11 20:14:12 +0000

    Adds put and delete methods

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,6 +12,38 @@ export async function get(endpoint) {
 		return { error: 'Internal Server Error: Network Error' };
 	}
 }
+export async function put(endpoint, bodyContent) {
+	try {
+		const request = await fetch(apiUrl + endpoint, requestSettings('PUT', bodyContent));
+
+		return request.status === 200;
+	} catch (error) {
+		return { error: 'Internal Server Error: Network Error' };
+	}
+}
+
+export async function deleteRequest(endpoint) {
+	try {
+		const request = await fetch(apiUrl + endpoint, requestSettings('DELETE'), null);
+
+		return request.status === 200;
+	} catch (error) {
+		return { error: 'Internal Server Error: Network Error' };
+	}
+}
+
+function requestSettings(requestType, bodyContent) {
+	const requestSettings = {
+		method: requestType,
+		headers: { 'Content-Type': 'application/json' },
+	};
+
+	if (bodyContent) {
+		requestSettings['body'] = JSON.stringify(bodyContent);
+	}
+
+	return requestSettings;
+}
 
 export function isNullOrEmpty(str) {
 	return str === null || str === '';
