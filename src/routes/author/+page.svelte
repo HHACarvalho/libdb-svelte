1ff878ffbf40dc72442bae2df2bb46fd136e03f5
Hugo Carvalho <hhac@protonmail.com>
2024-11-16 16:06:24 +0000

    Fixes the bug where the app lost track of the page number and size

--- src/routes/author/+page.svelte
+++ src/routes/author/+page.svelte
@@ -3,11 +3,12 @@
 	import ItemDisplay from '$lib/item-display.svelte';
 	import Pagination from '$lib/pagination.svelte';
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 
 	export let data;
-	const searchParams = new URLSearchParams();
-	let pageNumber = 1;
-	let pageSize = 16;
+	const searchParams = new URLSearchParams($page.url.searchParams);
+	let pageNumber = searchParams.get('pageNumber') || 1;
+	let pageSize = searchParams.get('pageSize') || 16;
 
 	function simpleSearchSubmit(event) {
 		searchParams.set('name', event.detail);
